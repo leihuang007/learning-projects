cf5df5bb08cd68e4765ef64143e472f5b616057f
leihuang007 <leihuang007@qq.com>
2023-05-29 22:25:55 +0800

    8.var.js

---
+++ JS-Bootcamp/03.Objects/8.var.js
@@ -0,0 +1,19 @@
+var firstName = 'Andrew'
+// like let, you can reassign a var variable
+firstName = 'Mike'
+
+// However, you can also redeclare a variable
+var firstName = 63
+
+// var is function scoped not block scoped.
+if (10 === 10) {
+    var tempName = 'Jen'
+}
+console.log(tempName)
+
+// You can also access a var variable before it is defined.
+console.log(varAge)
+var varAge
+
+console.log(letAge)
+let letAge
