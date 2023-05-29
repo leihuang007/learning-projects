033c3ac2df0c2779d84f087b99a207017a10ab29
leihuang007 <leihuang007@qq.com>
2023-05-29 21:53:55 +0800

    6.number-method.js

---
+++ JS-Bootcamp/03.Objects/6.number-method.js
@@ -0,0 +1,13 @@
+let num = 103.946
+
+console.log(num.toFixed(2))
+
+console.log(Math.round(num))
+// console.log(Math.floor(num))
+// console.log(Math.ceil(num))
+
+
+let min = 10
+let max = 20
+let randomNum = Math.floor(Math.random() * (max - min + 1)) + min
+console.log(randomNum)
